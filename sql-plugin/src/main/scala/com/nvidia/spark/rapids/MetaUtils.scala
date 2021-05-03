1d59fe0b0624d174a6def6f23fff9319a59b99d2
Alessandro Bellina <abellina@nvidia.com>
2021-05-03 12:15:21 -0500

    More format cleaning

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/MetaUtils.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/MetaUtils.scala
@@ -250,8 +250,7 @@ object ShuffleMetadata extends Logging{
     fbb.dataBuffer()
   }
 
-  def buildShuffleMetadataRequest(executorId: Long,
-                                  blockIds : Seq[ShuffleBlockBatchId]) : ByteBuffer = {
+  def buildShuffleMetadataRequest(blockIds : Seq[ShuffleBlockBatchId]) : ByteBuffer = {
     val fbb = new FlatBufferBuilder(1024, bbFactory)
     val blockIdOffsets = blockIds.map { blockId =>
       BlockIdMeta.createBlockIdMeta(fbb,
@@ -261,7 +260,7 @@ object ShuffleMetadata extends Logging{
         blockId.endReduceId)
     }
     val blockIdVectorOffset = MetadataRequest.createBlockIdsVector(fbb, blockIdOffsets.toArray)
-    val finIndex = MetadataRequest.createMetadataRequest(fbb, executorId, blockIdVectorOffset)
+    val finIndex = MetadataRequest.createMetadataRequest(fbb, blockIdVectorOffset)
     fbb.finish(finIndex)
     fbb.dataBuffer()
   }
@@ -303,10 +302,7 @@ object ShuffleMetadata extends Logging{
     fbb.dataBuffer()
   }
 
-  def buildTransferRequest(
-      localExecutorId: Long,
-      responseTag: Long,
-      toIssue: Seq[(TableMeta, Long)]): ByteBuffer = {
+  def buildTransferRequest(toIssue: Seq[(TableMeta, Long)]): ByteBuffer = {
     val fbb = ShuffleMetadata.getBuilder
     val requestIds = new ArrayBuffer[Int](toIssue.size)
     toIssue.foreach { case (tableMeta, tag) =>
@@ -317,8 +313,7 @@ object ShuffleMetadata extends Logging{
           tag))
     }
     val requestVec = TransferRequest.createRequestsVector(fbb, requestIds.toArray)
-    val transferRequestOffset = TransferRequest.createTransferRequest(fbb, localExecutorId,
-        responseTag, requestVec)
+    val transferRequestOffset = TransferRequest.createTransferRequest(fbb, requestVec)
     fbb.finish(transferRequestOffset)
     fbb.dataBuffer()
   }
