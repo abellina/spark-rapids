0b7f59214438459d3b1a5ab3b3b3d5ca372218ef
Alessandro Bellina <abellina@nvidia.com>
2023-04-05 20:28:50 -0500

    undo issue in stringFunctions

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/stringFunctions.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/stringFunctions.scala
@@ -762,12 +762,11 @@ trait HasGpuStringReplace extends Arm {
     strExpr: GpuColumnVector,
     search: Seq[String],
     replacement: String): ColumnVector = {
-    null
-     //withResource(ColumnVector.fromStrings(search: _*)) { targets =>
-     //  withResource(ColumnVector.fromStrings(replacement)) {  repls =>
-     //    strExpr.getBase.stringReplace(targets, repls)
-     //  }
-     //}
+     withResource(ColumnVector.fromStrings(search: _*)) { targets =>
+       withResource(ColumnVector.fromStrings(replacement)) {  repls =>
+         strExpr.getBase.stringReplace(targets, repls)
+       }
+     }
   }
 }
 
